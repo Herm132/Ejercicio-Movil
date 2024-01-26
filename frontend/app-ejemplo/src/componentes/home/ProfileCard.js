534033cb86a25c56c6e23c07fe396f3080c617d9
Herm132 <64614155+Herm132@users.noreply.github.com>
2024-01-26 12:07:12 -0500

    first commit

---
+++ frontend/app-ejemplo/src/componentes/home/ProfileCard.js
@@ -0,0 +1,95 @@
+import React from "react";
+import {View, Text, StyleSheet, Image, Linking,TouchableWithoutFeedback} from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+
+const twitter = <Icon name={'twitter'} size={30} color={'black'}/>
+const facebook = <Icon name={'facebook'} size={30} color={'black'}/>
+const instagram = <Icon name={'instagram'} size={30} color={'black'}/>
+const linkedin = <Icon name={'linkedin'} size={30} color={'black'}/>
+const tiktok = <Icon name={'sitemap'} size={30} color={'black'}/>
+const github = <Icon name={'github'} size={30} color={'black'}/>
+
+const ProfileCard = () => {
+    const user = {
+        avatar: "https://img.freepik.com/foto-gratis/pintura-lago-montana-montana-al-fondo_188544-9126.jpg",
+        coverPhoto: "https://img.freepik.com/foto-gratis/majestuoso-pico-montana-tranquilo-paisaje-invernal-generado-ia_188544-15662.jpg",
+        name: "Harold Reyes"
+    }
+    return (
+        <View style={styles.container}>
+            <Image source={{uri: user.coverPhoto}} style={styles.coverPhoto}/>
+            <View style={styles.avatarContainer}>
+                <Image source={{uri: user.avatar}} style={styles.avatar}/>
+                <Text style={styles.name}>
+                    {user.name}
+                </Text>
+            </View>
+            <View style={styles.buttonContainer}>
+
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://facebook.com/')
+                }}>
+                    {facebook}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://twitter.com/')
+                }}>
+                    {twitter}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback style={{color: 'blue'}} onPress={() => {
+                    Linking.openURL('https://instagram.com/')
+                }}>
+                    {instagram}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>Linking.openURL('https://linkedin.com/')}>
+                    {linkedin}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>Linking.openURL('https://tiktok.com/')}>
+                    {tiktok}
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={()=>Linking.openURL('https://github.com/')}>
+                {github}
+                </TouchableWithoutFeedback>
+
+          
+
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '50%',
+        alignItems: 'center' //flex y grid
+    },
+    coverPhoto: {
+        width: '100%',
+        height: 200,
+        resizeMode: 'cover'
+
+    },
+    avatarContainer: {
+        alignItems: 'center',
+        marginTop: -75
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        borderRadius: 75,
+        borderWidth: 10,
+        borderColor: 'white'
+    },
+    name: {
+        marginTop: 15,
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 20,
+        width: '60%',
+        justifyContent: 'space-between'
+    }
+});
+export default ProfileCard
